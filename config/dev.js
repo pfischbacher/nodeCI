17d8794e36ecc875f0cc8a5c48a66c53c1094cdd
Philip Fischbacher <pfischbacher@gmail.com>
2020-02-18 10:16:45 +0900

    added config files

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://advnodecourse:oktC9BRN8jhOUWG9@cluster0-shard-00-00-wguf4.gcp.mongodb.net:27017,cluster0-shard-00-01-wguf4.gcp.mongodb.net:27017,cluster0-shard-00-02-wguf4.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
